15d1cb6db38946cdc0fb6e41670396478c86b5f2
Zanidean` <`AZanidean@mhc.ab.ca`>
2017-08-15 11:46:42 -0600

    Add App

---
+++ server.R
@@ -0,0 +1,102 @@
+library(shiny)
+library(sRa)
+library(air)
+library(ggrepel)
+
+shinyServer(function(input, output) {
+  
+  dataframe <- reactive({
+    
+    df <- get_enrolment(input$measure, input$var, input$inst, 
+                  remove.offshores = input$offshores,
+                  remove.continuingstudies = input$cs)
+    
+    drop <- df %>% 
+      group_by_(.dots = c(lapply(c(input$var), as.symbol))) %>% 
+      summarise(N = n()) %>% 
+      filter(N >= 5) %>% 
+      select(1)
+    
+    dataframe <- df %>% 
+      filter(.[[input$var]] %in% c(drop[[1]])) %>% 
+      mutate(Institution = input$inst)
+    
+    dataframe
+    })
+  
+  height <- dataframe()
+  height <- length(unique(height[[input$var]]))*350
+
+  output$xmr <- renderPlot({
+    whitetheme <- theme_bw() + 
+      theme(strip.background = element_rect(fill = NA, linetype = 0), 
+            panel.border = element_rect(color = NA), 
+            panel.spacing.y = unit(4, "lines"), 
+            panel.spacing.x = unit(2, "lines"), 
+            panel.grid.major = element_blank(), 
+            panel.grid.minor = element_line(colour = "#fff2f2"),
+
+            axis.text.y =  element_blank(),
+            axis.title.y = element_blank(),
+            axis.ticks.y = element_blank(), 
+            axis.ticks.x = element_line(),
+            text = element_text(family = "sans"),
+            axis.text.x = element_text(colour = "#000000", size = 17),
+            axis.title.x = element_text(size = 20, face = "bold"),
+            strip.text = element_text(size = 20),
+            title = element_text(size = 25))
+    
+    dataframe <- dataframe() %>% 
+      group_by_(.dots = c(lapply(c(input$var), as.symbol))) %>% 
+      do(xmR(., input$measure, recalc = T)) %>%
+      mutate(`Academic Year` = gsub("^.*?-", "", `Academic Year`),
+             `Academic Year` = as.numeric(`Academic Year`))
+    
+    df <- ggplot(dataframe,
+               aes(`Academic Year`, group = dataframe[[input$var]])) +
+        geom_line(aes(y = `Central Line`),
+                  size = 1, 
+                  linetype = "dotted", 
+                  na.rm = T) +
+        geom_line(aes(y = `Lower Natural Process Limit`), 
+                  color = "#d02b27",
+                  size = 1.05, 
+                  linetype = "dashed", 
+                  na.rm = T) +
+        geom_line(aes(y = `Upper Natural Process Limit`), color = "#d02b27",
+                  size = 1.05, 
+                  linetype = "dashed", 
+                  na.rm = T) +
+        geom_line(aes(y = dataframe[[input$measure]])) + 
+        geom_point(aes(y = dataframe[[input$measure]]), 
+                   size = 5.55, 
+                   color = "#000000") +
+        geom_point(aes(y = dataframe[[input$measure]]), 
+                   size = 4.5, 
+                   color = "#7ECBB5") +
+        ggrepel::geom_label_repel(aes(y = dataframe[[input$measure]]),
+                  label = round(dataframe[[input$measure]]),
+                  size = 6,
+                  label.padding = unit(0.2, "lines"),
+                  label.r = unit(0, "lines"),
+                  label.size = 0.15,
+                  box.padding = unit(1, "lines"),
+                  fill = "#ffffff") +
+        guides(colour=FALSE) +   
+        #ggtitle(input$var) + 
+        facet_wrap(~dataframe[[input$var]],
+                   nrow = length(unique(dataframe[[input$var]])),
+                   scales = "free",
+                   labeller = label_wrap_gen()) + whitetheme
+    df
+  }, height = height
+  })
+  output$downloadData <- downloadHandler(
+    filename = "Download.csv",
+    content = function(file) {
+      write.csv(dataframe(), file)
+    })
+
+  
+})
+
---
+++ ui.R
@@ -0,0 +1,46 @@
+library(shiny)
+library(sRa)
+library(air)
+library(ggrepel)
+
+shinyUI(fluidPage(
+  
+  titlePanel("Enrolment Explorer"),
+  tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, 
+                  .js-irs-0 .irs-bar {background: #7ECBB5}")),
+  sidebarLayout(
+    sidebarPanel(style = "max-height: 900px; position:relative;",
+                 selectInput("measure", "Measure", 
+                             choices = c("Unique Student Static", "FLE")),
+                 selectInput("inst", "Institution", 
+                             choices = c("Medicine Hat College" = "MH", 
+                                         "Mount Royal University" = "MU", 
+                                         "University of Alberta" = "UA", 
+                                         "Lethbridge College" = "LC", 
+                                         "University of Lethbridge" = "UL")),
+                 selectInput("var", "Variable", 
+                             choices = c("Gender", 
+                              "Legal Status", 
+                              "Aboriginal Indicator",
+                              "Year Of Study", 
+                              "Program Band", 
+                              "Age Group",
+                              "Level Of Study", 
+                              "Session", 
+                              "Current Status", 
+                              "Source Country",
+                              "Province")),
+                 radioButtons("offshores", "Remove offshores?", 
+                              choices = c("Yes" = TRUE, "No" = FALSE)),
+                 radioButtons("cs", "Remove continuing studies?",
+                              choices = c("Yes" = TRUE, "No" = FALSE)),
+                 downloadButton('downloadData', 'Download'),
+                 sliderInput("size", "Height of Charts",
+                             min = 300, max =3000, value = 850),
+    width = 2),
+  
+    mainPanel(
+       plotOutput("xmr", width = "auto")
+    )
+  )
+))
